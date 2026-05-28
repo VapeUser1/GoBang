80191d6f9d53955c0bd6c86d1aaab05f60d19de3
VapeUser <13641364334@163.com>
2026-05-28 15:53:38 +0800

    优化player class的代码

--- pycode/Player.py
+++ pycode/Player.py
@@ -7,31 +7,16 @@ class Player:
     def __init__(self, name, piece):
         self.name = name
         self.piece = piece
-class HumanPlayer(Player):
-    def getMove_cmd(self):
-        x, y = map(int, input("Enter your move (row and column): ").split())
-        return x, y
-    def getMove(self, gui):
-        # 在这里实现图形用户界面的获取用户输入的方法
-        pass
-#第一级AI：在密集位置随机选取位置
-class AIPlayer1(Player):
-    def getMove(self, board):
-        # 在这里实现AI获取用户输入的方法
-        empty_cells = board.getValuablePlace(2)
-        return random.choice(empty_cells)
-#第二级AI：在密集位置中根据分数判断选取最有利的位置
-class AIPlayer2(Player):
-    def getMove(self, board):
+    #2级ai使用算法
+    def getMoveByScore(self, board):
         empty_cells = board.getValuablePlace(3)
         # 根据分数选择最有利的位置
         scores = [AIfunction.get_score(board.board, x, y) for x, y in empty_cells]
         max_score = max(scores) if scores else 0
         best_moves = [cell for cell, score in zip(empty_cells, scores) if score == max_score]
         return random.choice(best_moves) if best_moves else random.choice(empty_cells)
-#第三级AI：考虑多步决策树
-class AIPlayer3(Player):
-    def getMove(self, board):
+    #3级ai使用算法
+    def getMoveByTree(self, board):
         empty_cells = board.getValuablePlace(3)
         for c in empty_cells:
             if AIfunction.get_score(board.board, c[0], c[1]) >= 1000000:
@@ -46,12 +31,9 @@ class AIPlayer3(Player):
         if best_move and max(best_move.evaluate() for best_move in pos) >= 30000:
             return best_move.latestMove
         # 如果没有更好的选择就按照2级ai的算法
-        scores = [AIfunction.get_score(board.board, x, y) for x, y in empty_cells]
-        max_score = max(scores) if scores else 0
-        best_moves = [cell for cell, score in zip(empty_cells, scores) if score == max_score]
-        return random.choice(best_moves) if best_moves else random.choice(empty_cells)
-class AIPlayer4(Player):
-    def getMove(self, board):
+        return self.getMoveByScore(board)
+    #4级ai使用算法
+    def getMoveByTreeWithExpand(self, board):
         empty_cells = board.getValuablePlace(3)
         for c in empty_cells:
             if AIfunction.get_score(board.board, c[0], c[1]) >= 1000000:
@@ -68,7 +50,29 @@ class AIPlayer4(Player):
         if best_move and max(best_move.evaluate() for best_move in pos) >= 30000:
             return best_move.latestMove
         # 如果没有更好的选择就按照2级ai的算法
-        scores = [AIfunction.get_score(board.board, x, y) for x, y in empty_cells]
-        max_score = max(scores) if scores else 0
-        best_moves = [cell for cell, score in zip(empty_cells, scores) if score == max_score]
-        return random.choice(best_moves) if best_moves else random.choice(empty_cells)
+        return self.getMoveByScore(board)
+
+class HumanPlayer(Player):
+    def getMove_cmd(self):
+        x, y = map(int, input("Enter your move (row and column): ").split())
+        return x, y
+    def getMove(self, gui):
+        # 在这里实现图形用户界面的获取用户输入的方法
+        pass
+#第一级AI：在密集位置随机选取位置
+class AIPlayer1(Player):
+    def getMove(self, board):
+        # 在这里实现AI获取用户输入的方法
+        empty_cells = board.getValuablePlace(2)
+        return random.choice(empty_cells)
+#第二级AI：在密集位置中根据分数判断选取最有利的位置
+class AIPlayer2(Player):
+    def getMove(self, board):
+        return self.getMoveByScore(board)
+#第三级AI：考虑多步决策树
+class AIPlayer3(Player):
+    def getMove(self, board):
+        return self.getMoveByTree(board)
+class AIPlayer4(Player):
+    def getMove(self, board):
+        return self.getMoveByTreeWithExpand(board)
